9d26bc82bc43553fbecee1972ea43f223cfa55cb
itheima <1305975814@qq.com>
2022-05-07 11:02:47 +0800

    第四次

--- springBoot+EasyPOI/src/main/java/com/helllo/demo/controller/DemoController.java
+++ springBoot+EasyPOI/src/main/java/com/helllo/demo/controller/DemoController.java
@@ -3,19 +3,13 @@ package com.helllo.demo.controller;
 
 import cn.afterturn.easypoi.excel.ExcelExportUtil;
 import cn.afterturn.easypoi.excel.entity.ExportParams;
-import com.helllo.demo.mapper.StudentMapper;
 import com.helllo.demo.pojo.Student;
 import com.helllo.demo.service.StudentService;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.STUnderline;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
@@ -23,9 +17,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/hello")
 public class DemoController {
-
-//    @Autowired
-//    private StudentMapper mapper;
 
     @Autowired
     private StudentService service;
